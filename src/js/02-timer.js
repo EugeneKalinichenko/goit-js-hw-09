4490f266b45f163533b6e36aa57930b8dda8ea73
EugeneKalinichenko <e.o.kalinichenko1994@gmail.com>
2022-02-20 18:38:57 +0200

    task 2 done

---
+++ src/js/02-timer.js
@@ -0,0 +1,93 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import '../css/common.css';
+
+let intervalId = null;
+let selectedDate = null;
+let timerValue = null;
+
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+refs.startBtn.disabled = true;
+
+const timer = {
+  start() {
+    intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      timerValue = selectedDate - currentTime;
+      console.log(timerValue);
+      if (timerValue <= 0) { 
+        timer.end();
+      }
+      updateTimerValue(convertMs(timerValue))
+    }, 1000);
+  },
+  end() { 
+    clearInterval(intervalId);
+    refs.startBtn.disabled = true;
+    window.alert('Please restart this tab to see small magic ones again:)');
+        timerValue = 0;
+  },
+};
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    selectedDate = selectedDates[0].getTime();
+    if (selectedDate < Date.now()) {
+      window.alert("Please choose a date in the future")
+    } else {
+      refs.startBtn.disabled = false;
+    }
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+};
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+};
+
+function updateTimerValue({ days, hours, minutes, seconds }) {
+  refs.days.textContent = days;
+  refs.hours.textContent = hours;
+  refs.minutes.textContent = minutes;
+  refs.seconds.textContent = seconds;
+}
+
+refs.startBtn.addEventListener('click', timer.start);
+
+document.addEventListener('keydown', evt => {
+  if (evt.code === 'KeyQ') {
+    console.log('You shall not pass!')
+    timer.end();
+  }
+});
